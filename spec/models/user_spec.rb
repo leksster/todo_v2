942b6a40e6dcd7db2b8a2aa2d1dce05bf6afb597
Alex Smith <leksster@gmail.com>
2016-04-09 00:12:54 +0300

    NOT!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { create(:user) }
 
+  it { should have_many(:projects).dependent(:destroy) }
+  it { should have_many(:tasks) }
+
+  it { should validate_uniqueness_of(:email).case_insensitive }
 end
